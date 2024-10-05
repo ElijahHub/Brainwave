9d7cc065a12c2dd64ab93ac718a7509540bd8668
Elijah <elijahadewunmi2005@gmail.com>
2024-10-05 22:09:38 +0100

    last commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefit from "./components/Benefit";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import RoadMap from "./components/RoadMap";
+import Services from "./components/Services";
+
+const App = () => {
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+        <Hero />
+        <Benefit />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <RoadMap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
